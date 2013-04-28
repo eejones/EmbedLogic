84104484b4fb42c21e6b8d370c53942a89b17815
Eric Jones <joneserice@gmail.com>
2013-04-28 01:42:26 -0500

    Added additional video uploading services for switching to the embed code

--- lib/embed_logic/core_ext.rb
+++ lib/embed_logic/core_ext.rb
@@ -3,7 +3,12 @@
 String.class_eval do
   def embedded
     oldcode=self.dup
-    newcode="http://www.youtube.com/embed/" + (oldcode.match(/(?:http:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/))[1]+ "?feature=player_detailpage"
+    if oldcode =~ /(?:youtube\.com|youtu\.be)/
+      newcode="http://www.youtube.com/embed/" + (oldcode.match(/(?:http:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/))[1]+ "?feature=player_detailpage"
+    elsif oldcode =~ /(?:vimeo)/
+      newcode="http://player.vimeo.com/video/25076865"
+#     newcode="http://player.vimeo.com/video/"+ (oldcode.match(/(?:http:\/\/)?(?:www\.)?(?:vimeo\.com)\/?(.+)/))[1]
+    end
     newcode
   end
 end
